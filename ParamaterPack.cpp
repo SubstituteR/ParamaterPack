02cf9116e53fb87cfd08f1f7272a456c8653fe36
Riley S <Github@rileystrickland.com>
2020-05-19 16:09:41 -0400

    Add files via upload

---
+++ ParamaterPack.cpp
@@ -0,0 +1,70 @@
+// ParamaterPack.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+
+struct Packed
+{
+    char* parameters;
+    char* returnValue;
+    int size;
+};
+
+template<typename ...Ts> int totalSize(Ts&&... args)
+{
+    int size = 0;
+    char unpack[] = { ([&size](auto& item) {
+            size += sizeof(item);
+        }(args), 0)..., 0 };
+    static_cast<void>(unpack);
+    return size;
+}
+
+template<typename ...Ts> Packed f(Ts&&... args)
+{
+    Packed packed;
+    packed.size = totalSize(args...);
+    char* memoryBlock = (char*) calloc(1, packed.size);
+    packed.parameters = memoryBlock;
+    char copymemory[] = { ([&memoryBlock, &packed](auto& item) {
+            memcpy(memoryBlock, &item, sizeof(item));
+            packed.returnValue = memoryBlock;
+            memoryBlock += sizeof(item); //move pointer forward
+        }(args), 0)..., 0 };
+    static_cast<void>(copymemory);
+    return packed;
+}
+
+struct MyStruct
+{
+    int a;
+    float b;
+    int ret;
+};
+
+void test(MyStruct* data)
+{
+    std::cout << "my data is " << data->a << " and " << data->b << "\n";
+    data->ret = 69;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    Packed block =  f(100, 10.f,0);
+    ((void(*)(void*))test)(block.parameters); //just to call by a function pointer
+    printf("Returned value %i\n", *block.returnValue);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
